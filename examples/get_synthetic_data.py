60458c64b13af5352ee8abb179eacb2c23c8b248
greg2451 <51173502+greg2451@users.noreply.github.com>
2023-01-11 19:22:35 +0100

    feat: add plot_2 generation

--- examples/get_synthetic_data.py
+++ examples/get_synthetic_data.py
@@ -1,15 +1,28 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from random import seed, shuffle, sample
+from random import sample
 from scipy.stats import multivariate_normal
 from scipy.stats import norm as univariate_normal
+from sklearn.linear_model import (
+    LinearRegression,
+)
+from examples.utils import (
+    compute_fairness_measures,
+    get_ddp_linear,
+    get_ddp_convex_concave,
+    get_ddp_upper,
+    get_ddp_lower,
+    normalize_by_scaling,
+)
+from tqdm import tqdm
+from itertools import product
 
-plt.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+
+plt.rc("font", **{"family": "sans-serif", "sans-serif": ["Helvetica"]})
 ## for Palatino and other serif fonts use:
-plt.rc('font',**{'family':'serif','serif':['Palatino']})
-plt.rcParams['text.usetex'] = True
+plt.rc("font", **{"family": "serif", "serif": ["Palatino"]})
+# plt.rcParams['text.usetex'] = True
 
 SMALL_SIZE = 37
 MEDIUM_SIZE = 40
@@ -18,18 +31,21 @@ BIGGER_SIZE = 45
 MARKER_SIZE = 200
 CROSS_SIZE = 3000
 
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=MEDIUM_SIZE, labelsize=BIGGER_SIZE)     # fontsize of the axes title
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=MEDIUM_SIZE)  # fontsize of the figure title
+plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+plt.rc(
+    "axes", titlesize=MEDIUM_SIZE, labelsize=BIGGER_SIZE
+)  # fontsize of the axes title
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=MEDIUM_SIZE)  # fontsize of the figure title
+
 
 def get_gaussian_data(n_samples=None, plot_data=False):
     """
-        Code for generating the synthetic data.
-        We will have two non-sensitive features and one sensitive feature.
-        A sensitive feature value of -1 means the example is considered to be in protected group (e.g., female) and 1.0 means it's in non-protected group (e.g., male).
+    Code for generating the synthetic data.
+    We will have two non-sensitive features and one sensitive feature.
+    A sensitive feature value of -1 means the example is considered to be in protected group (e.g., female) and 1.0 means it's in non-protected group (e.g., male).
     """
     if n_samples is None:
         n_samples = 1000  # generate these many data points per class
@@ -47,28 +63,43 @@ def get_gaussian_data(n_samples=None, plot_data=False):
 
     """ Generate the non-sensitive features randomly """
     # We will generate one gaussian cluster for each class
-    mu1, sigma1 = [2, -2], [[1, 0], [0, 1]]  # negative class, negative sens attr (protected)
-    mu2, sigma2 = [4.5, -1.5], [[1, 0], [0, 1]]  # negative class, positive sens attr (unprotected)
+    mu1, sigma1 = [2, -2], [
+        [1, 0],
+        [0, 1],
+    ]  # negative class, negative sens attr (protected)
+    mu2, sigma2 = [4.5, -1.5], [
+        [1, 0],
+        [0, 1],
+    ]  # negative class, positive sens attr (unprotected)
 
     mu3, sigma3 = [3, -1], [[1, 0], [0, 1]]  # positive class, positive sens attr
     mu32, sigma32 = [1, 4], [[0.5, 0], [0, 0.5]]  # positive class, positive sens attr
-    mu4, sigma4 = [2.5, 2.5], [[1, 0], [0, 1]] # positive class, negative sens attr
+    mu4, sigma4 = [2.5, 2.5], [[1, 0], [0, 1]]  # positive class, negative sens attr
 
-    nmb_1, nmb_2 = int(np.floor(n_samples/ 4)), int(np.floor(n_samples/ 8))
+    nmb_1, nmb_2 = int(np.floor(n_samples / 4)), int(np.floor(n_samples / 8))
 
     gap = n_samples - 3 * nmb_1 - 2 * nmb_2
 
-    nv1, X1, y1, s1 = gen_gaussian(mu1, sigma1, -1, -1,  nmb_1)  # negative class, negative sens attr
-    nv2, X2, y2, s2 = gen_gaussian(mu2, sigma2, -1, 1, nmb_1)  # negative class, positive sens attr
+    nv1, X1, y1, s1 = gen_gaussian(
+        mu1, sigma1, -1, -1, nmb_1
+    )  # negative class, negative sens attr
+    nv2, X2, y2, s2 = gen_gaussian(
+        mu2, sigma2, -1, 1, nmb_1
+    )  # negative class, positive sens attr
 
-    nv3, X3, y3, s3 = gen_gaussian(mu3, sigma3, 1, -1, nmb_2)  # positive class, negative sens attr
-    nv32, X32, y32, s32 = gen_gaussian(mu32, sigma32, 1, -1, nmb_2)  # positive class, negative sens attr
+    nv3, X3, y3, s3 = gen_gaussian(
+        mu3, sigma3, 1, -1, nmb_2
+    )  # positive class, negative sens attr
+    nv32, X32, y32, s32 = gen_gaussian(
+        mu32, sigma32, 1, -1, nmb_2
+    )  # positive class, negative sens attr
     X3 = np.vstack((X3, X32))
     y3 = np.hstack((y3, y32))
     s3 = np.hstack((s3, s32))
 
-    nv4, X4, y4, s4 = gen_gaussian(mu4, sigma4, 1, 1, nmb_1+gap)  # positive class, positive sens attr
-
+    nv4, X4, y4, s4 = gen_gaussian(
+        mu4, sigma4, 1, 1, nmb_1 + gap
+    )  # positive class, positive sens attr
 
     # join the posisitve and negative class clusters
     x_data = np.vstack((X1, X2, X3, X4))
@@ -83,17 +114,19 @@ def get_gaussian_data(n_samples=None, plot_data=False):
     s_data = s_data[perm]
 
     if plot_data:
-        plot_data_(x_data, y_data, s_data, num_to_draw=400)
+        plot_data_(x_data, y_data, s_data)
 
     return x_data, y_data, s_data
 
 
-def plot_data_(x_data,y_data,s_data, num_to_draw=False):
+def plot_data_(x_data, y_data, s_data, num_to_draw=False):
     if not num_to_draw:
-        num_to_draw = x_data.shape[0]  # we will only draw a small number of points to avoid clutter
+        num_to_draw = x_data.shape[
+            0
+        ]  # we will only draw a small number of points to avoid clutter
 
     cmap_points = "coolwarm_r"
-    fig, ax = plt.subplots(figsize=(10,10))
+    fig, ax = plt.subplots(figsize=(10, 10))
 
     x_draw = x_data[:num_to_draw]
     y_draw = y_data[:num_to_draw]
@@ -104,10 +137,50 @@ def plot_data_(x_data,y_data,s_data, num_to_draw=False):
     y_s_0 = y_draw[s_draw == -1.0]
     y_s_1 = y_draw[s_draw == 1.0]
 
-    ax.scatter(X_s_0[y_s_0 == 1.0][:, 0], X_s_0[y_s_0 == 1.0][:, 1], c=np.ones_like(y_s_0[y_s_0 == 1.0]), cmap=cmap_points, alpha=0.9, marker='+', s=MARKER_SIZE, linewidth=2.5, norm=MidpointNormalize(midpoint=0,vmax=1,vmin=-1))
-    ax.scatter(X_s_0[y_s_0 == -1.0][:, 0], X_s_0[y_s_0 == -1.0][:, 1], c=np.ones_like(y_s_0[y_s_0 == -1.0]), cmap=cmap_points, alpha=0.9,marker='_', s=MARKER_SIZE, linewidth=2.5, norm=MidpointNormalize(midpoint=0,vmax=1, vmin=-1))
-    ax.scatter(X_s_1[y_s_1 == 1.0][:, 0], X_s_1[y_s_1 == 1.0][:, 1], c=-1*np.ones_like(y_s_1[y_s_1 == 1.0]), cmap=cmap_points, alpha=0.9,marker='+', s=MARKER_SIZE, linewidth=2.5, norm=MidpointNormalize(midpoint=0,vmax=1,vmin=-1))
-    ax.scatter(X_s_1[y_s_1 == -1.0][:, 0], X_s_1[y_s_1 == -1.0][:, 1], c=-1*np.ones_like(y_s_1[y_s_1 == -1.0]), cmap=cmap_points,alpha=0.9, marker='_', s=MARKER_SIZE, linewidth=2.5, norm=MidpointNormalize(midpoint=0,vmax=1,vmin=-1))
+    ax.scatter(
+        X_s_0[y_s_0 == 1.0][:, 0],
+        X_s_0[y_s_0 == 1.0][:, 1],
+        c=np.ones_like(y_s_0[y_s_0 == 1.0]),
+        cmap=cmap_points,
+        alpha=0.9,
+        marker="+",
+        s=MARKER_SIZE,
+        linewidth=2.5,
+        norm=MidpointNormalize(midpoint=0, vmax=1, vmin=-1),
+    )
+    ax.scatter(
+        X_s_0[y_s_0 == -1.0][:, 0],
+        X_s_0[y_s_0 == -1.0][:, 1],
+        c=np.ones_like(y_s_0[y_s_0 == -1.0]),
+        cmap=cmap_points,
+        alpha=0.9,
+        marker="_",
+        s=MARKER_SIZE,
+        linewidth=2.5,
+        norm=MidpointNormalize(midpoint=0, vmax=1, vmin=-1),
+    )
+    ax.scatter(
+        X_s_1[y_s_1 == 1.0][:, 0],
+        X_s_1[y_s_1 == 1.0][:, 1],
+        c=-1 * np.ones_like(y_s_1[y_s_1 == 1.0]),
+        cmap=cmap_points,
+        alpha=0.9,
+        marker="+",
+        s=MARKER_SIZE,
+        linewidth=2.5,
+        norm=MidpointNormalize(midpoint=0, vmax=1, vmin=-1),
+    )
+    ax.scatter(
+        X_s_1[y_s_1 == -1.0][:, 0],
+        X_s_1[y_s_1 == -1.0][:, 1],
+        c=-1 * np.ones_like(y_s_1[y_s_1 == -1.0]),
+        cmap=cmap_points,
+        alpha=0.9,
+        marker="_",
+        s=MARKER_SIZE,
+        linewidth=2.5,
+        norm=MidpointNormalize(midpoint=0, vmax=1, vmin=-1),
+    )
 
     plt.locator_params(nbins=5)
     ax.set_xlabel(r"$x_1$")
@@ -117,11 +190,13 @@ def plot_data_(x_data,y_data,s_data, num_to_draw=False):
 
 def plot_boundaries(model, x_data, y_data, s_data, num_to_draw=False):
     if not num_to_draw:
-        num_to_draw = x_data.shape[0]  # we will only draw a small number of points to avoid clutter
+        num_to_draw = x_data.shape[
+            0
+        ]  # we will only draw a small number of points to avoid clutter
 
     cmap_points = "coolwarm_r"
-    cmap_contour = plt.get_cmap('RdYlGn')
-    fig, ax = plt.subplots(figsize=(10,10))
+    cmap_contour = plt.get_cmap("RdYlGn")
+    fig, ax = plt.subplots(figsize=(10, 10))
 
     x_draw = x_data[:num_to_draw]
     y_draw = y_data[:num_to_draw]
@@ -132,28 +207,70 @@ def plot_boundaries(model, x_data, y_data, s_data, num_to_draw=False):
     y_s_0 = y_draw[s_draw == -1.0]
     y_s_1 = y_draw[s_draw == 1.0]
 
-    ax.scatter(X_s_0[y_s_0 == 1.0][:, 0], X_s_0[y_s_0 == 1.0][:, 1], c=np.ones_like(y_s_0[y_s_0 == 1.0]), cmap=cmap_points, alpha=1, marker='+', s=MARKER_SIZE, linewidth=2.5, norm=MidpointNormalize(midpoint=0,vmax=1,vmin=-1))
-    ax.scatter(X_s_0[y_s_0 == -1.0][:, 0], X_s_0[y_s_0 == -1.0][:, 1], c=np.ones_like(y_s_0[y_s_0 == -1.0]), cmap=cmap_points, alpha=1,marker='_', s=MARKER_SIZE, linewidth=2.5, norm=MidpointNormalize(midpoint=0,vmax=1, vmin=-1))
-    ax.scatter(X_s_1[y_s_1 == 1.0][:, 0], X_s_1[y_s_1 == 1.0][:, 1], c=-1*np.ones_like(y_s_1[y_s_1 == 1.0]), cmap=cmap_points, alpha=1,marker='+', s=MARKER_SIZE, linewidth=2.5, norm=MidpointNormalize(midpoint=0,vmax=1,vmin=-1))
-    ax.scatter(X_s_1[y_s_1 == -1.0][:, 0], X_s_1[y_s_1 == -1.0][:, 1], c=-1*np.ones_like(y_s_1[y_s_1 == -1.0]), cmap=cmap_points,alpha=1, marker='_', s=MARKER_SIZE, linewidth=2.5, norm=MidpointNormalize(midpoint=0,vmax=1,vmin=-1))
+    ax.scatter(
+        X_s_0[y_s_0 == 1.0][:, 0],
+        X_s_0[y_s_0 == 1.0][:, 1],
+        c=np.ones_like(y_s_0[y_s_0 == 1.0]),
+        cmap=cmap_points,
+        alpha=1,
+        marker="+",
+        s=MARKER_SIZE,
+        linewidth=2.5,
+        norm=MidpointNormalize(midpoint=0, vmax=1, vmin=-1),
+    )
+    ax.scatter(
+        X_s_0[y_s_0 == -1.0][:, 0],
+        X_s_0[y_s_0 == -1.0][:, 1],
+        c=np.ones_like(y_s_0[y_s_0 == -1.0]),
+        cmap=cmap_points,
+        alpha=1,
+        marker="_",
+        s=MARKER_SIZE,
+        linewidth=2.5,
+        norm=MidpointNormalize(midpoint=0, vmax=1, vmin=-1),
+    )
+    ax.scatter(
+        X_s_1[y_s_1 == 1.0][:, 0],
+        X_s_1[y_s_1 == 1.0][:, 1],
+        c=-1 * np.ones_like(y_s_1[y_s_1 == 1.0]),
+        cmap=cmap_points,
+        alpha=1,
+        marker="+",
+        s=MARKER_SIZE,
+        linewidth=2.5,
+        norm=MidpointNormalize(midpoint=0, vmax=1, vmin=-1),
+    )
+    ax.scatter(
+        X_s_1[y_s_1 == -1.0][:, 0],
+        X_s_1[y_s_1 == -1.0][:, 1],
+        c=-1 * np.ones_like(y_s_1[y_s_1 == -1.0]),
+        cmap=cmap_points,
+        alpha=1,
+        marker="_",
+        s=MARKER_SIZE,
+        linewidth=2.5,
+        norm=MidpointNormalize(midpoint=0, vmax=1, vmin=-1),
+    )
 
     # Create a mesh of points to plot in
     # h: Stepsize of meshgrid
     h = 0.2
-    x_min, x_max = x_draw[:,0].min() - 1, x_draw[:,0].max() + 1
-    y_min, y_max = x_draw[:,1].min() - 1, x_draw[:,1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
+    x_min, x_max = x_draw[:, 0].min() - 1, x_draw[:, 0].max() + 1
+    y_min, y_max = x_draw[:, 1].min() - 1, x_draw[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
-    #Plot the decision boundaries for a classifier.
+    # Plot the decision boundaries for a classifier.
     zz = np.sign(model.predict(np.c_[xx.ravel(), yy.ravel()]))
     zz = zz.reshape(xx.shape)
-    ax.contourf(xx, yy, zz, alpha=0.3, cmap=cmap_contour, norm=MidpointNormalize(midpoint=0))
+    ax.contourf(
+        xx, yy, zz, alpha=0.3, cmap=cmap_contour, norm=MidpointNormalize(midpoint=0)
+    )
 
     plt.locator_params(nbins=5)
     ax.set_xlabel(r"$x_1$")
     ax.set_ylabel(r"$x_2$")
     plt.show()
+
 
 # set the colormap and centre the colorbar
 class MidpointNormalize(mcolors.Normalize):
@@ -162,6 +279,7 @@ class MidpointNormalize(mcolors.Normalize):
 
     e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
     """
+
     def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
         self.midpoint = midpoint
         mcolors.Normalize.__init__(self, vmin, vmax, clip)
@@ -171,7 +289,58 @@ class MidpointNormalize(mcolors.Normalize):
         return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
 
 
-if __name__ == "__main__":
+def get_figure_2(
+    n_samples=500,
+    coef_max=2,
+    intercept_max=4,
+    step=0.05,
+):
+    x, y, s = get_gaussian_data(n_samples=n_samples, plot_data=True)
+    # No training so all is test data
+    x_test, y_test, s_test = x, y, s
+    model = LinearRegression()
+    coefficients = np.arange(-coef_max, coef_max + step / 2, step)
+    intercepts = np.arange(-intercept_max, intercept_max + step / 2, step)
+    empirical_ddp = np.zeros((len(coefficients), len(intercepts)))
+    linear_ddp = np.zeros((len(coefficients), len(intercepts)))
+    convex_concave_ddp = np.zeros((len(coefficients), len(intercepts)))
+    lower_ddp = np.zeros((len(coefficients), len(intercepts)))
+    upper_ddp = np.zeros((len(coefficients), len(intercepts)))
+    for coefficient, intercept in tqdm(product(coefficients, intercepts)):
+        model.intercept_ = intercept
+        model.coef_ = np.array([coefficient, -1])
+        y_reg = model.predict(x_test)
+        y_pred = np.array([1 if y > 0 else -1 for y in y_reg])
+        i, j = int((coefficient + coef_max) / step), int(
+            (intercept + intercept_max) / step
+        )
+        ddp, _ = compute_fairness_measures(y_pred, y_test, -s_test)
+        empirical_ddp[i, j] = ddp
+        linear_ddp[i, j] = get_ddp_linear(y_reg, -s_test)
+        convex_concave_ddp[i, j] = get_ddp_convex_concave(y_reg, -s_test)
+        lower_ddp[i, j] = get_ddp_lower(y_reg, -s_test)
+        upper_ddp[i, j] = get_ddp_upper(y_reg, -s_test)
 
-    n = 600
-    x_train, y_train, s_train = get_gaussian_data(n_samples=n, plot_data=True)
+    ddp_relaxations = {
+        "DDP": normalize_by_scaling(empirical_ddp),
+        "Linear DDP": normalize_by_scaling(linear_ddp),
+        "Convex-Concave DDP": normalize_by_scaling(convex_concave_ddp),
+        "Lower DDP": normalize_by_scaling(lower_ddp),
+        "Upper DDP": normalize_by_scaling(upper_ddp),
+    }
+    fig, ax = plt.subplots(ncols=5, figsize=(70, 10))
+    y, x = np.meshgrid(coefficients, intercepts)
+
+    for i, (title, z) in enumerate(ddp_relaxations.items()):
+        z = z.T
+        z = z[:-1, :-1]
+        c = ax[i].pcolormesh(x, y, z, cmap="RdYlBu_r", vmin=-1, vmax=1)
+        ax[i].set_title(title)
+        ax[i].axis([x.min(), x.max(), y.min(), y.max()])
+
+    fig.colorbar(c, ax=ax.ravel().tolist())
+    plt.show()
+
+
+if __name__ == "__main__":
+    get_figure_2(n_samples=2000, step=0.05)
--- examples/utils.py
+++ examples/utils.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
-from collections import defaultdict
-import itertools
 from sklearn.metrics import confusion_matrix
+
 
 def print_data_stats(sens_attr, class_labels):
     """Print a few numbers about the data: Total number of points, number of
@@ -18,7 +17,9 @@ def print_data_stats(sens_attr, class_labels):
     """
     non_prot_all = sum(sens_attr == 1.0)  # non-protected group
     prot_all = len(sens_attr) - non_prot_all  # protected group
-    non_prot_pos = sum(class_labels[sens_attr == 1.0] == 1.0)  # non_protected in positive class
+    non_prot_pos = sum(
+        class_labels[sens_attr == 1.0] == 1.0
+    )  # non_protected in positive class
     prot_pos = sum(class_labels == 1.0) - non_prot_pos  # protected in positive class
     frac_non_prot_pos = float(non_prot_pos) / float(non_prot_all)
     frac_prot_pos = float(prot_pos) / float(prot_all)
@@ -26,8 +27,15 @@ def print_data_stats(sens_attr, class_labels):
     print("Total data points: %d" % len(sens_attr))
     print("# non-protected examples: %d" % non_prot_all)
     print("# protected examples: %d" % prot_all)
-    print("# non-protected examples in positive class: %d (%0.1f%%)" % (non_prot_pos, non_prot_pos * 100.0 / non_prot_all))
-    print("# protected examples in positive class: %d (%0.1f%%)" % (prot_pos, prot_pos * 100.0 / prot_all))
+    print(
+        "# non-protected examples in positive class: %d (%0.1f%%)"
+        % (non_prot_pos, non_prot_pos * 100.0 / non_prot_all)
+    )
+    print(
+        "# protected examples in positive class: %d (%0.1f%%)"
+        % (prot_pos, prot_pos * 100.0 / prot_all)
+    )
+
 
 def get_positive_rate(y_predicted, y_true):
     """Compute the positive rate for given predictions of the class label.
@@ -45,8 +53,9 @@ def get_positive_rate(y_predicted, y_true):
         The positive rate.
     """
     tn, fp, fn, tp = confusion_matrix(y_true, y_predicted).ravel()
-    pr = (tp+fp) / (tp+fp+tn+fn)
+    pr = (tp + fp) / (tp + fp + tn + fn)
     return pr
+
 
 def get_true_positive_rate(y_predicted, y_true):
     """Compute the true positive rate for given predictions of the class label.
@@ -64,8 +73,9 @@ def get_true_positive_rate(y_predicted, y_true):
         The true positive rate.
     """
     tn, fp, fn, tp = confusion_matrix(y_true, y_predicted).ravel()
-    tpr = tp / (tp+fn)
+    tpr = tp / (tp + fn)
     return tpr
+
 
 def compute_fairness_measures(y_predicted, y_true, sens_attr):
     """Compute value of demographic parity and equality of opportunity for given predictions.
@@ -86,14 +96,174 @@ def compute_fairness_measures(y_predicted, y_true, sens_attr):
     DEO: float
         The difference of equality of opportunity.
     """
-    positive_rate_prot = get_positive_rate(y_predicted[sens_attr==-1], y_true[sens_attr==-1])
-    positive_rate_unprot = get_positive_rate(y_predicted[sens_attr==1], y_true[sens_attr==1])
-    true_positive_rate_prot = get_true_positive_rate(y_predicted[sens_attr==-1], y_true[sens_attr==-1])
-    true_positive_rate_unprot = get_true_positive_rate(y_predicted[sens_attr==1], y_true[sens_attr==1])
+    positive_rate_prot = get_positive_rate(
+        y_predicted[sens_attr == -1], y_true[sens_attr == -1]
+    )
+    positive_rate_unprot = get_positive_rate(
+        y_predicted[sens_attr == 1], y_true[sens_attr == 1]
+    )
+    true_positive_rate_prot = get_true_positive_rate(
+        y_predicted[sens_attr == -1], y_true[sens_attr == -1]
+    )
+    true_positive_rate_unprot = get_true_positive_rate(
+        y_predicted[sens_attr == 1], y_true[sens_attr == 1]
+    )
     DDP = positive_rate_unprot - positive_rate_prot
     DEO = true_positive_rate_unprot - true_positive_rate_prot
 
     return DDP, DEO
+
+
+def get_ddp_linear(
+    y_reg,
+    sens_attr,
+):
+    """Compute the demographic parity for given predictions.
+
+    Parameters
+    ----------
+    y_reg: numpy array
+        The value of f on x of shape=(number_points,).
+    sens_attr: numpy array
+        The sensitive labels of shape=(number_points,).
+
+    Returns
+    ---------
+    DDP: float
+        The linear relaxation of demographic parity as defined by Donini et al. (2018).
+    """
+    n = len(y_reg)
+    p1_hat = (sens_attr == 1).sum() / n
+    return (
+        1
+        / n
+        * np.sum(
+            [
+                sens_attr[i]
+                * y_reg[i]
+                / (p1_hat if sens_attr[i] == 1.0 else 1.0 - p1_hat)
+                for i in range(n)
+            ]
+        )
+    )
+
+
+def get_ddp_convex_concave(y_reg, sens_attr):
+    """Compute the demographic parity for given predictions.
+
+    Parameters
+    ----------
+    y_reg: numpy array
+        The value of f on x of shape=(number_points,).
+    sens_attr: numpy array
+        The sensitive labels of shape=(number_points,).
+
+    Returns
+    ---------
+    DDP: float
+        The convex concave relaxation of demographic parity as defined by Zafar et al. (2017a).
+    """
+    n = len(y_reg)
+    p1_hat = (sens_attr == 1).sum() / n
+    return (
+        1
+        / n
+        * np.sum(
+            [
+                ((sens_attr[i] + 1 / 2) - p1_hat)
+                * max(0, y_reg[i])
+                / (p1_hat * (1.0 - p1_hat))
+                for i in range(n)
+            ]
+        )
+    )
+
+
+def get_ddp_upper(y_reg, sens_attr):
+    """Compute the demographic parity for given predictions.
+
+    Parameters
+    ----------
+    y_reg: numpy array
+        The value of f on x of shape=(number_points,).
+    sens_attr: numpy array
+        The sensitive labels of shape=(number_points,).
+
+    Returns
+    ---------
+    DDP: float
+        The upper relaxation of demographic parity as defined by Wu et al. (2019).
+    """
+    n = len(y_reg)
+    p1_hat = (sens_attr == 1).sum() / n
+    kappa = lambda x: max(0, 1 + x)
+    return (
+        1
+        / n
+        * np.sum(
+            [
+                (
+                    kappa(y_reg[i]) / p1_hat
+                    if sens_attr[i] == 1.0
+                    else kappa(-y_reg[i]) / (1.0 - p1_hat)
+                )
+                - 1
+                for i in range(n)
+            ]
+        )
+    )
+
+
+def get_ddp_lower(y_reg, sens_attr):
+    """Compute the demographic parity for given predictions.
+
+    Parameters
+    ----------
+    y_reg: numpy array
+        The value of f on x of shape=(number_points,).
+    sens_attr: numpy array
+        The sensitive labels of shape=(number_points,).
+
+    Returns
+    ---------
+    DDP: float
+        The lower relaxation of demographic parity as defined by Wu et al. (2019).
+    """
+    n = len(y_reg)
+    p1_hat = (sens_attr == 1).sum() / n
+    delta = lambda x: min(1, x)
+    return (
+        1
+        / n
+        * np.sum(
+            [
+                (
+                    delta(y_reg[i]) / p1_hat
+                    if sens_attr[i] == 1.0
+                    else delta(-y_reg[i]) / (1.0 - p1_hat)
+                )
+                - 1
+                for i in range(n)
+            ]
+        )
+    )
+
+
+def normalize_by_scaling(matrix: np.array):
+    """Normalize the matrix by scaling the values to be between -1 and 1.
+
+    Parameters
+    ----------
+    matrix: numpy array
+        The matrix to be normalized.
+
+    Returns
+    ----------
+    matrix: numpy array
+        The normalized matrix.
+    """
+    return matrix / np.max(np.abs(matrix))
+
 
 def get_accuracy(y_true, y_predicted):
     """Compute the accuracy for given predicted class labels.
@@ -110,6 +280,8 @@ def get_accuracy(y_true, y_predicted):
     accuracy: float
         The accuracy of the predictions.
     """
-    correct_answers = (y_predicted == y_true).astype(int)  # will have 1 when the prediction and the actual label match
+    correct_answers = (y_predicted == y_true).astype(
+        int
+    )  # will have 1 when the prediction and the actual label match
     accuracy = float(sum(correct_answers)) / float(len(correct_answers))
     return accuracy
